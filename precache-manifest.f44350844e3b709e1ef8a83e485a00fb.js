7bfdb97fa6764f4cc56d384da2792cdb63892d83
Ghibli <aran.t.ali@gmail.com>
2021-07-05 23:37:49 -0400

    Updates

---
+++ precache-manifest.f44350844e3b709e1ef8a83e485a00fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25055ff78b63448d621ede24e61a0a7",
+    "url": "/https//aranata.github.io/book-search-engine/index.html"
+  },
+  {
+    "revision": "e41c7fa426d93bf6d746",
+    "url": "/https//aranata.github.io/book-search-engine/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "fdf346feb60f4fa581c1",
+    "url": "/https//aranata.github.io/book-search-engine/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "e41c7fa426d93bf6d746",
+    "url": "/https//aranata.github.io/book-search-engine/static/js/2.f2bee1c0.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/https//aranata.github.io/book-search-engine/static/js/2.f2bee1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf346feb60f4fa581c1",
+    "url": "/https//aranata.github.io/book-search-engine/static/js/main.63ebed97.chunk.js"
+  },
+  {
+    "revision": "165716a1ce0183a27715",
+    "url": "/https//aranata.github.io/book-search-engine/static/js/runtime-main.04626497.js"
+  }
+]);
